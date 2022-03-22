91d51092a1f7fcfb68a3ddae4bdd1c01f62eb54a
zhoukaikai <2516361678@qq.com>
2022-03-22 20:39:51 +0800

    添加项目文件。

---
+++ WebApiRedis/Controllers/CountController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiRedis.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CountController : ControllerBase
+    {
+        private const string s_key = "count";
+        private readonly ILogger<CountController> _logger;
+        private readonly IDistributedCache _cache;
+        public CountController(ILogger<CountController> logger, IDistributedCache cache)
+        {
+            _logger = logger;
+            _cache = cache;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            string value = _cache.GetString(s_key);
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                _cache.SetString(s_key, "1");
+            }
+            else
+            {
+                int intValue = Convert.ToInt32(value) + 1;
+                _cache.SetString(s_key, intValue.ToString());
+            }
+            return _cache.GetString(s_key);
+        }
+    }
+}
